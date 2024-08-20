7c3f75884c34afa62d9c74e6b57807a99aeec2b6
Utku Cengiz <84972875+UTKUC3NGIZ@users.noreply.github.com>
2024-08-20 23:21:43 +0300

    arrow button

--- src/components/buttons/arrowButton.js
+++ src/components/buttons/arrowButton.js
@@ -1,25 +1,24 @@
 "use client";
-
 import React from "react";
 import MyIcon from "@/../public/assets/shared/desktop/arrow.svg";
 
-function ArrowButton({ color }) {
+function arrowButton({ color }) {
   const buttonStyle = {
     white: "text-pureWhite",
     black: "text-pureBlack",
   };
-
   return (
     <div>
       <button
-        className={`uppercase font-bold text-xs tracking-[2px] hover:underline
+        className={`uppercase font-bold text-xs tracking-[2px] hover:underline flex flex-row items-center gap-4
              ${buttonStyle[color]}`}
       >
         Button 1
+        <MyIcon className={`${buttonStyle[color]}`} />
       </button>
-      <img src={MyIcon} alt="Arrow Icon" />
+      <div></div>
     </div>
   );
 }
 
-export default ArrowButton;
+export default arrowButton;
