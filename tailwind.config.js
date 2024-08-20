4c7b436c963249fb70de4b66a09ecc9eb9bcfdc2
Utku Cengiz <84972875+UTKUC3NGIZ@users.noreply.github.com>
2024-08-20 22:21:14 +0300

    install

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,12 @@ module.exports = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    colors: {
+      main: "#ffffff",
+      pureBlack: "#000000",
+      pureWhite: "#ffffff",
+      lightGrey: "#DFDFDF",
+    },
   },
   plugins: [],
 };
