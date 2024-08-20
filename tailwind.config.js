08116f40d94b41fd4ac70ad36c39d237dd233b63
Utku Cengiz <84972875+UTKUC3NGIZ@users.noreply.github.com>
2024-08-20 22:38:48 +0300

    fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,12 +12,32 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-    },
-    colors: {
-      main: "#ffffff",
-      pureBlack: "#000000",
-      pureWhite: "#ffffff",
-      lightGrey: "#DFDFDF",
+      colors: {
+        main: "#ffffff",
+        pureBlack: "#000000",
+        pureWhite: "#ffffff",
+        lightGrey: "#DFDFDF",
+        custom: {
+          peach: "#FFC593",
+          mauve: "#BC7198",
+          blue: "#5A77FF",
+        },
+      },
+      gradientColorStops: (theme) => ({
+        "custom-gradient-start": "#FFC593",
+        "custom-gradient-middle": "#BC7198",
+        "custom-gradient-end": "#5A77FF",
+      }),
+      fontSize: {
+        "h1-h2": ["40px", { lineHeight: "48px" }],
+        h2: ["24px", { lineHeight: "25px" }],
+        h3: ["18px", { lineHeight: "25px" }],
+        h4: ["12px", { lineHeight: "16px" }],
+        body: ["15px", { lineHeight: "25px" }],
+      },
+      letterSpacing: {
+        tight: "-0.05em",
+      },
     },
   },
   plugins: [],
