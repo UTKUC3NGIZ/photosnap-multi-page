acd4b85d7deb082a769405f259d8027a8c0b3f2a
Utku Cengiz <84972875+UTKUC3NGIZ@users.noreply.github.com>
2024-08-21 04:03:28 +0300

    tablet fix

--- src/components/footer.js
+++ src/components/footer.js
@@ -10,12 +10,12 @@ import ArrowButton from "@/components/buttons/arrowButton";
 
 function footer() {
   return (
-    <footer className="bg-pureBlack py-14 px-8 grid grid-cols-1 justify-items-center">
-      <div>
-        <Link href={""} className="pb-8 block">
+    <footer className="bg-pureBlack py-14 px-8 grid grid-cols-1 justify-items-center md:flex md:flex-row md:justify-between">
+      <div className="flex flex-col">
+        <Link href={""} className="pb-8 block md:order-1">
           <Logo className="text-pureWhite" />
         </Link>
-        <div className="flex flex-row gap-3 pb-12 justify-center">
+        <div className="flex flex-row gap-3 pb-12 justify-center md:order-3 md:justify-start">
           <Link href={""}>
             <Facebook className="text-pureWhite" />
           </Link>
@@ -32,16 +32,16 @@ function footer() {
             <Instagram className="text-pureWhite" />
           </Link>
         </div>
-        <ul className="text-center gap-5 flex flex-col pb-32 text-xs font-bold tracking-[2px] uppercase">
+        <ul className="text-center gap-5 flex flex-col pb-32 text-xs font-bold tracking-[2px] uppercase md:flex-row md:order-2 md:pb-[72px]">
           <li>Home</li>
           <li>Stories</li>
           <li>Features</li>
           <li>Pricing</li>
         </ul>
       </div>
-      <div className="flex flex-col items-center">
+      <div className="flex flex-col items-center md:justify-between md:items-end">
         <ArrowButton />
-        <div className="pt-8">
+        <div className="pt-8 md:pt-0 md:pb-12">
           <p className="text-base leading-5 text-pureWhite text-opacity-50">
             &copy; Copyright 2024. All Rights Reserved
           </p>
